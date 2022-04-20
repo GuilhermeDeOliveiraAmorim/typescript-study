785a77fc10b3cdf98d7e528053f737401af23ebd
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-04-20 13:13:14 -0300

    Condition to working days of the week in method adiciona in NegociacaoController

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,9 +20,14 @@ export class NegociacaoController {
 
   public adiciona(): void {
     const negociacao = this.criaNegociacao();
-    this.negociacoes.adiciona(negociacao);
-    this.limparFormulario();
-    this.atualizaView();
+    /*getDay(): 0-dom, ..., 6-sab*/
+    if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
+      this.negociacoes.adiciona(negociacao);
+      this.limparFormulario();
+      this.atualizaView();
+    } else {
+      this.mensagemView.update("Apenas em dias úteis!");
+    }
   }
 
   private criaNegociacao(): Negociacao {

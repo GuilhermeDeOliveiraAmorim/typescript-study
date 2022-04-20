92bcbd349500ebd9cd42548997483e535813eeaa
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-04-20 12:39:07 -0300

    Add protected for abstract class

--- app/views/view.ts
+++ app/views/view.ts
@@ -10,6 +10,6 @@ export abstract class View<T> {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
-    
-    abstract template(model: T): string;
+
+    protected abstract template(model: T): string;
 }
